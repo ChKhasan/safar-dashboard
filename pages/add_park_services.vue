32af422acecb03a0c76fa7a1f7b537176f19b932
ChKhasan <chulliyevhasan1405@mail.ru>
2024-03-28 16:26:10 +0500

    lang fix

--- pages/add_park_services.vue
+++ pages/add_park_services.vue
@@ -858,10 +858,10 @@ export default {
           label: "O'zbek",
           index: "uz",
         },
-        // {
-        //   label: "English",
-        //   index: "en",
-        // },
+        {
+          label: "English",
+          index: "en",
+        },
       ],
       editorOption: {
         // Some Quill options...
